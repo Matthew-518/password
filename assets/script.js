dc2cbc8773146938df2179f97fa4b83a80d058d3
Matt Hill <hill2420@gmail.com>
2021-09-06 20:47:07 -0400

    js confirm

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,19 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
+  var passwordLength = prompt("How long?"); 
+  // if passwordLength = 
+  
+  var lowerCase = confirm("lowercase?");
+
+  var uppercase = confirm("uppercase?");
+  var numeric = confirm("numeric");
+  var specialChar = confirm("special character");
+
+ 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
