e287ab0cc86d95b4c7635550d5e0ae3f699f9d38
Matt Hill <hill2420@gmail.com>
2021-09-09 12:32:57 -0400

    changed password length vaidation

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,5 @@
 // Assignment code here
 
-
 var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
 var numeric =  ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
@@ -9,24 +8,37 @@ var SpecialChar =  ['+','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']',
 var passwordText = document.querySelector("#password");
 var generateBtn = document.querySelector("#generate");
  
-var generatePassword = function() {
-  var password = "";
+//var generatePassword = function() {
+  //var password = "";
   //return getPassword;
-}
+//}
 
 function writePassword() {
     
-  //passwordText.value = getPassword
-
   var getPassword = [];
   
   var chosenCharacters = [];
 
-  var passwordLength = prompt("How Long?");
-  if(passwordLength < 8 || passwordLength > 128) {
-    return alert("try again");
+  // Variable to store length of password from user input
+  var passwordLength = parseInt(
+    prompt('How many characters would you like your password to contain?')
+  );
+  // Conditional statement to check if password length is a number. Prompts end if this evaluates false
+  if (Number.isNaN(passwordLength)) {
+    alert('Password length must be provided as a number');
+    return null;
   }
-  
+  // Conditional statement to check if password length is at least 8 characters long. Prompts end if this evaluates false
+  if (passwordLength < 8) {
+    alert('Password length must be at least 8 characters');
+    return null;
+  }
+  // Conditional statement to check if password length is less than 128 characters long. Prompts end if this evaluates false
+  if (passwordLength > 128) {
+    alert('Password length must less than 129 characters');
+    return null;
+  }
+ 
   var useLowerCase = confirm("lowercase?");
   if (useLowerCase === true) {
       chosenCharacters = chosenCharacters.concat(lowerCase);
@@ -66,3 +78,8 @@ generateBtn.addEventListener("click", writePassword);
  //removeComma();
   //generatePassword();
    //console.log(getPassword);
+
+    //var passwordLength = prompt("How Long?");
+  //if(passwordLength < 8 || passwordLength > 128) {
+    //return alert("try again");
+  //}
